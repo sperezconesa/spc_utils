befe007d7afa7b23967f5189e80bf010360188f8
sergio <sergio.perez.conesa@scilifelab.se>
2020-09-17 14:14:49 +0200

    Added some doc strings to spc_os.py.

--- spc_os.py
+++ spc_os.py
@@ -5,6 +5,9 @@ and avoid annoing things of os.
 import os, errno
 
 def remove(filename):
+    '''
+    Call remove of os and not give error if file does not exist.
+    '''
     try:
         os.remove(filename)
     except OSError as e: # this would be "except OSError, e:" before Python 2.6
@@ -12,4 +15,7 @@ def remove(filename):
         if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
             raise # re-raise exception if a different error occurred
 def mkdir(filename):
+    '''
+    Call makedirs of os and not give error if path exists
+    '''
     os.makedirs(filename, exist_ok=True)

da28f5afb4573c248eb1356024f2496ac903a78d
sergio <sergio.perez.conesa@scilifelab.se>
2020-11-16 18:24:32 +0100

    Fixed bug Janin angle function for valine residues. Also moved here the dssp function

--- spc_analysis.py
+++ spc_analysis.py
@@ -11,14 +11,15 @@ def mda_janin_with_CSTV(u,selection):
       ----------
       u: MDAnalysis universe.
       selection: selection of the universe containing one or several aminoacids
-                sharing resid and resname. 
+                sharing resid and resname.
       Returns
       -------
       output_angles: numpy array of angles, shape[n_frames, n_dihedrals, n_subunits]
     '''
-    
-    
     from MDAnalysis.analysis.dihedrals import Janin
+    from MDAnalysis import Universe
+    import numpy as np
+
     substitute = {
         'CYS': 'SG',
         'SER': 'OG',
@@ -28,7 +29,7 @@ def mda_janin_with_CSTV(u,selection):
     resname = np.unique(selection.resnames)
     resid = np.unique(selection.resids)
     assert resname.shape[0] != 1 or resid.shape[0] == 1, 'more than one resid'
-    assert isinstance(u, mda.Universe) , 'u should be a MDAnlaysis universe.'
+    assert isinstance(u, Universe) , 'u should be a MDAnlaysis universe.'
     if resname[0] in ['CYS', 'SER', 'THR', 'VAL']:
         my_list = []
         for res in selection.residues:
@@ -106,3 +107,52 @@ def get_rmsd(u, ref=None, sel_str='name CA and protein', skip=1,sel_str_al=None,
         os.remove('tmp.pdb')
         os.remove('tmp.xtc')
     return rmsd,np.array(t0)
+def get_dssp(path,path_top,simplified=True):
+    '''
+    Calculate and plot the secondary structure as a function of time and split per subunit.
+    
+    Parameters
+    ----------
+    path: path to structure file.
+    path_top: path to topology file
+    simplified: Should the structure types be simplified.
+      
+    Returns
+    -------
+    fig: matplotlib figure object.
+    ax: axes of the figures.
+    '''
+    import mdtraj as md
+    import MDAnalysis as mda
+    import os
+    import matplotlib.pyplot as plt
+    
+    assert isinstance(simplified,bool),'Simplified should be boulean.'
+    assert os.path.isfile(path),'Structure path does not exisist.'
+    assert os.path.isfile(path_top),'Topology path does not exisist.'
+    
+    traj=mda.Universe(path_top,path)
+    u=md.load(path,top=path_top,atom_indices=traj.select_atoms("backbone").atoms.indices)
+    dssp=md.compute_dssp(u,simplified)
+    time =np.linspace(0,traj.trajectory[-1].time,traj.trajectory.n_frames)
+    time = time/1000
+    n=traj.select_atoms("protein").n_segments
+    resid=np.split(traj.select_atoms("name CA and protein").resids,n)[0]
+    if simplified:
+        ss=["H","E","C"]
+        ss_name=["H","B","C"]
+    else:
+        ss=["H","B","E","I",'T','S','']
+        ss_name=[r'$\alpha$',r'$\beta$ Bridge',r'$\beta$ Ladder','3/10',r'$\pi$','Turn','Bend','Coil']     
+    dssp0=np.zeros(dssp.shape)
+    for i,code in enumerate(ss):
+        dssp0[dssp==code]=i
+    fig,ax = plt.subplots(n,1,figsize = (17,5*n))
+    k=[]
+    for segid,a in zip(np.split(dssp0.T,4),ax.flat):
+        k.append(a.pcolor(time,resid,segid,cmap=plt.cm.get_cmap("tab10", len(ss))))
+        formatter = plt.FuncFormatter(lambda val, loc: ss_name[val])
+        a.set_xlabel("t (ns)")
+        a.set_ylabel("resid")
+    fig.colorbar(k[-1],ax=ax,ticks=range(len(ss)),format=formatter)
+    return fig,ax

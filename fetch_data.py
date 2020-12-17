db1b3142f8d0cc23b37324e2987c61187d0247b6
sperezconesa <sperezconesa@gmail.com>
2020-12-17 20:13:49 +0100

    Several stuff for example, added ppc plotter.

--- fetch_data.py
+++ fetch_data.py
@@ -51,3 +51,27 @@ def gsheet_to_csv_url(gsheet_name,worksheet,csv_filename):
     worksheet=sh.worksheet(worksheet)
     DataFrame(worksheet.get_all_records()).to_csv(csv_filename)
     return
+def df_to_data_sheet(df, gsheet_name, worksheet):
+    '''
+    Send  dataframe df to worksheet in gsheet_name google sheet.  
+    
+    Parameters
+    ----------
+    def: dataframe  to send to gsheets. 
+    gsheet_name: name of the spreadsheet.
+    worksheet: name of the worksheet.
+      
+    Returns
+    -------
+    '''
+    from gspread import service_account
+    from pandas import DataFrame
+
+    assert isinstance(df,DataFrame), 'df is not a pandas DataFrame'
+    assert isinstance(gsheet_name,str), 'gsheet_name is not a string' 
+    assert isinstance(worksheet,str), 'gsheet_name is not a string' 
+    gc = service_account()
+    sh = gc.open(gsheet_name)
+    worksheet=sh.worksheet(worksheet)
+    worksheet.update([df.columns.values.tolist()] + df.values.tolist())
+    return
--- spc_vis.py
+++ spc_vis.py
@@ -19,7 +19,7 @@ def format_plus_minus_error(value, error, format_value='.1f', format_error='.1f'
     assert isinstance(format_error,str), 'Format error is not a string.'
     assert isinstance(value, Iterable) and all(isinstance(x, float) for x in value), 'Value is not an iterable of floats.'
     assert isinstance(error, Iterable) and all(isinstance(x, float) for x in error), 'Error is not an iterable of floats.'
-    string = '{v:' + format_value + '} \u00B1 {e:' + format_error + '}'
+    string = '{v:' + format_value + '}\u00B1{e:' + format_error + '}'
     plus_minus = [ string.format(v=v, e=e) for v, e in zip(value,error) ]
     return plus_minus
 def fasta(filename):
@@ -175,3 +175,57 @@ def AnnotateResidues(path, ax, x_lims, fontfamily=None, fontsize=None, H_color='
 
     ax.set_xlim([x_lims[0], x_lims[1]])
     return ax
+def my_plot_ppc(trace, label_array, **kwargs):
+    '''
+    Plots the posterior predictive check associated to a arviz inference object in which
+    the model's data has been structured with dataframe encoding during the model building
+    like the "finches" example of scipy. It is important that the data should be obtained
+    with pm.sample_posterior_predictive(my_model_trace,  random_seed=RANDOM_SEED, samples=nsamples, size=nsize).
+      Parameters
+      ----------
+      trace: arviz inference object containing observed_data and posterior_predicitive.
+      label_array: array or dataframe with the labels of the observed data.
+      Returns
+      -------
+      fig, ax: matplolib figures and axes.
+    '''
+    from numpy import unique,array
+    from matplotlib.pyplot import subplots
+    from arviz import plot_ppc
+    from arviz import InferenceData
+    from collections.abc import Iterable
+
+    assert isinstance(label_array, Iterable) and all(isinstance(x, int) for x in label_array) and all(label_array >=0), 'label_array is not an iterable of non-negative ints.'
+    assert isinstance(trace,InferenceData), 'trace is not an arviz inference object'
+    key = list(trace.posterior_predictive.data_vars.keys())
+    assert len(key) == 1, 'There should be only one data_var in trace.posterior_predicitve'
+    key = list(trace.observed_data.data_vars.keys())
+    assert len(key) == 1, 'There should be only one data_var in trace.posterior_predicitve'
+    key=key[0]
+
+    allowed_plot_kwargs =['alpha']
+    plot_kwargs ={k:kwargs.pop(k) for k,v in list(kwargs.items()) if k in allowed_plot_kwargs}
+
+    labels = unique(label_array)
+    n_plots = labels.shape[0]
+    n_rows=n_plots//4+1
+    if n_rows == 1:
+        sizex = 7.5 * n_plots
+        n_cols = n_plots
+    else:
+        sizex = 5*4
+    fig, ax = subplots(n_rows, n_cols, figsize=(sizex,5.5*n_rows), sharex=True, sharey=True)
+    ax = ax.flatten()
+    for i in range(n_plots):
+        trace.observed_data['ppc'] = trace.observed_data[key][array(label_array) == i]
+        trace.posterior_predictive['ppc'] = trace.posterior_predictive[key][:,:,:,i]
+        plot_ppc(trace, var_names = 'ppc', legend=False, ax=ax[i], **plot_kwargs)
+        ax[i].set_xlabel(f'ppc var {i}')
+    ax[0].legend()
+    if n_rows > 1:
+        for i in range(4 - n_plots % 4):
+            fig.delaxes(ax[-i-1])
+    fig.tight_layout()
+    trace.posterior_predictive = trace.posterior_predictive.drop('ppc')
+    trace.observed_data = trace.observed_data.drop('ppc')
+    return fig, ax
